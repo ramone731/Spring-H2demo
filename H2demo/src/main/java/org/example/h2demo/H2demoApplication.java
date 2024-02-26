2c88b56c65ae91faec010c75147f6e6f59f30875
rl <74465003+ramone731@users.noreply.github.com>
2024-02-25 21:40:23 -0500

    Add files via upload

---
+++ H2demo/src/main/java/org/example/h2demo/H2demoApplication.java
@@ -0,0 +1,16 @@
+package org.example.h2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class H2demoApplication {
+
+    public static void main(String[] args) {
+
+      SpringApplication.run(H2demoApplication.class, args);
+
+    }
+
+}
---
+++ H2demo/src/test/java/org/example/h2demo/H2demoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.h2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2demoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
